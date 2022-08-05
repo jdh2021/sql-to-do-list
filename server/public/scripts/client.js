39cc6d14071e8bc4e8f6ce5feee08db6cb3e3741
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-08-04 19:32:31 -0500

    created inputfield and button. created click event handler for button. initial css styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,13 @@ $(readyNow);
 
 function readyNow() {
     console.log('in readyNow, jQuery sourced');
+    $('#task-button').on('click', addTask);
+}
+
+const taskToPost = {};
+
+function addTask() {
+    console.log('in addTask');
+    taskToPost.description = $('#task-input').val();
+    console.log(taskToPost);
 }
