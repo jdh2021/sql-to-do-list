ce64add3f1ee0e6c3f9606bfcb0efb2b90312c7f
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-08-09 16:47:53 -0500

    testing moment.js server-side to get time task added

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 
 const taskArray = [];
 
 router.post('/', (req, res) => {
     console.log('in router POST');
+    let currentTime = moment().format('LT'); //testing moment.js for time task added
+    console.log(currentTime);
     const taskToPost = req.body;
     taskArray.push(taskToPost);
     console.log(taskArray);
